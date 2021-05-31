0ddea853cef4034cba905ff0c6fb18793a77585e
CarlieDixon <cdixon83@gmail.com>
2021-05-30 21:06:53 -0400

    add js to change images in mobile view

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,4 @@
+// hamburger nav function
 const menu = document.querySelector(".menu");
 const ham = document.querySelector(".ham");
 const hamNav = document.querySelector(".ham-nav")
@@ -24,3 +25,56 @@ const toggleMenu = function () {
 }
 
 ham.addEventListener("click", toggleMenu);
+
+
+// to flip through the images in mobile view
+let n = 1
+
+const image1 = document.querySelector(".image1");
+const image2 = document.querySelector(".image2");
+const image3 = document.querySelector(".image3");
+
+const next = document.querySelector('.next')
+const back = document.querySelector('.back')
+
+const nextImage = function () {
+    if (n === 1) {
+        image1.classList.remove("showImage");
+        image1.classList.add("hideImage");
+        image2.classList.add("showImage");
+        n += 1;
+    } else if (n === 2) {
+        image2.classList.remove("showImage");
+        image2.classList.add("hideImage");
+        image3.classList.add("showImage");
+        n += 1;
+    } else if (n === 3){
+        image3.classList.remove("showImage");
+        image3.classList.add("hideImage");
+        image1.classList.add("showImage");
+        n = 1;
+    }
+}
+
+next.addEventListener("click", nextImage);
+
+const backImage = function () {
+    if (n === 3) {
+        image3.classList.remove("showImage");
+        image3.classList.add("hideImage");
+        image2.classList.add("showImage");
+        n -= 1;
+    } else if (n === 2) {
+        image2.classList.remove("showImage");
+        image2.classList.add("hideImage");
+        image1.classList.add("showImage");
+        n -= 1;
+    } else if (n === 1){
+        image1.classList.remove("showImage");
+        image1.classList.add("hideImage");
+        image3.classList.add("showImage");
+        n = 3;
+    }
+}
+
+back.addEventListener("click", backImage);
